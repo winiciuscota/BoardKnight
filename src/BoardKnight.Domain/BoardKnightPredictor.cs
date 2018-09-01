9881c20d256dd7bb9410a693f24bae007a01164c
Winicius Iago Vieira Cota <winicius.cota@gmail.com>
2018-09-01 20:15:59 -0300

    Initial project structure, using TDD

---
+++ src/BoardKnight.Domain/BoardKnightPredictor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoardKnight.Domain
+{
+    public class BoardKnightPredictor
+    {
+        public IEnumerable<ChessPosition> GetPossibleMoves(ChessPosition position)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
